e33c335508acf1d397cd8b883ad64bfcca121dec
Anthony Sena <asena5@its.jnj.com>
2022-08-22 14:24:39 -0400

    Revise SettingsFunctions.R

--- SettingsFunctions.R
+++ SettingsFunctions.R
@@ -1,11 +1,6 @@
 createPatientLevelPredictionModuleSpecifications <- function(
   modelDesignList
 ) {
-  #analysis <- list()
-  #for (name in names(formals(createCohortDiagnosticsModuleSpecifications))) {
-  #  analysis[[name]] <- get(name)
-  #}
-  
   specifications <- list(
     module = "PatientLevelPredictionModule",
     version = "0.0.1-1",
